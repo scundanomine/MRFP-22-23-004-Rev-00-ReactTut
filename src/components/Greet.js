40b5c93a2a604fc3cbefed9bb3fb5c3d5d9b4a32
unknown <scundanomine21@gmail.com>
2022-10-02 05:37:07 +0530

    woe

---
+++ src/components/Greet.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export const Greet = ()=> {
+    return(
+        <>
+        <h1>Hello Jeeshan</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque ea quibusdam ratione minima. Fuga, voluptatem.</p>
+        </>
+    );
+}
+
+// export default Greet;
